a2011a28237b61f1674a48f795f34d4639b8276a
Stewart Watkiss <stewart@watkissonline.co.uk>
2017-01-15 22:38:09 +0000

    Work in progress on comms

--- arduino/networking_quiz/networking_quiz_ino/networking_quiz_ino.ino
+++ arduino/networking_quiz/networking_quiz_ino/networking_quiz_ino.ino
@@ -14,8 +14,25 @@
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 int delayval = 500; // delay for half a second
 
+
+// Serial communications setup
+int portStatus[6] = {0,0,0,0,0,0};    // which value sent
+int inByte = 0;         // incoming serial byte
+int ledStatus[6] = {0,0,0,0,0,0};
+
+
+
 void setup() {
   pixels.begin(); // This initializes the NeoPixel library.
+  
+  
+    // start serial port at 9600 bps and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  establishContact();  // send a byte to establish contact until receiver responds 
+    
 }
 
 void loop() {
@@ -32,7 +49,36 @@ void loop() {
     delay(delayval); // Delay for a period of time (in milliseconds).
 
   }
+  
+  
+  
 }
+
+
+void communicate () {
+  while (1) {
+      if (Serial.available() > 0) {
+      // wait until receive 254
+      inByte = Serial.read();
+      if (inByte != 254) continue;
+      
+      // Must have received 254 - so read next 6 values
+      for (int i=0; i<6; i++) {
+          ledStatus[i] = Serial.read();
+      }
+    
+      Serial.write(254);
+      Serial.write(0);  
+      Serial.write(0);
+      Serial.write(34);
+      Serial.write(35);
+      Serial.write(36);
+      Serial.write(37);
+    }  
+    
+  }
+}
+
 
 // set led - status 0 = off, 1 = green, 2 = red, 3 = blue (searching)
 void setStatus (int lednum, int status) {
@@ -50,3 +96,16 @@ void setStatus (int lednum, int status) {
  }
 }
 
+
+void establishContact() {
+  while (1) {
+    Serial.write(255);   // send an initial string
+    delay(300);
+    // read and check for a 255 indicating that the pi is ready
+    if (Serial.available() > 0) {
+      inByte = Serial.read();
+      if (inByte == 255) return;
+      Serial.write(inByte);
+    }
+  }
+}
--- src/network-quiz.py
+++ src/network-quiz.py
@@ -2,21 +2,43 @@
 
 import serial
 import time
+import struct
 
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=5)
+ser = serial.Serial('/dev/ttyACM3', 9600, timeout=5)
 
 # read from Arduino
 input = ser.read()
-print ("Read input " + input.decode("utf-8") + " from Arduino")
+#print ("Read input " + input.decode("utf-8") + " from Arduino")
+# Initial response is 255 = ready to send
+ser.write(struct.pack('!B',255))
 
 while 1:
         # write something back
-        ser.write(b'A')
+        #ser.write(b'A')
+        ser.write(struct.pack('!B',254))
+        ser.write(struct.pack('!B',3))
+        ser.write(struct.pack('!B',2))
+        ser.write(struct.pack('!B',1))
+        ser.write(struct.pack('!B',0))
+        ser.write(struct.pack('!B',2))
+        ser.write(struct.pack('!B',3))
+
+        # Now clear read buffer until get 254
+        while 1:
+                input = ser.read()
+                input_number = ord(input)
+                print ("Clearing buffer " + str(input_number))
+                if (input_number == 255) : 
+                        ser.write(struct.pack('!B',255))
+                if (input_number == 254) : 
+                        break
+		
 
         # read response back from Arduino
-        for i in range (0,3):
+        for i in range (0,6):
                 input = ser.read()
                 input_number = ord(input)
                 print ("Read input back: " + str(input_number))
+                #print ("Read input back: " + input)
 
-        time.sleep(1)
+        time.sleep(5)
