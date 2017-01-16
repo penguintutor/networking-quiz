75e58a24cbb3ed44f6fafc88ac0890d20fb9db00
Stewart Watkiss <stewart@watkissonline.co.uk>
2017-01-16 19:23:57 +0000

    Initial comms working

--- arduino/networking_quiz/networking_quiz.ino
+++ arduino/networking_quiz/networking_quiz.ino
@@ -1,49 +1,138 @@
+// Networking Quiz
 
 
-// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
-// released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
-
-#include "Adafruit_NeoPixel/Adafruit_NeoPixel.h"
+#include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
 
+// NeoPixel Setup
 // Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1
 #define PIN            10
-
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS      16
-
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
+#define NUMPIXELS      12
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+int delayval = 50; // delay for half a second
 
-int delayval = 500; // delay for half a second
+
+// Serial communications setup
+int portStatus[6] = {0,0,0,0,0,0};    // which value sent
+int inByte = 0;         // incoming serial byte
+int ledStatus[6] = {0,0,0,0,0,0};
+int commStatus = 0;    // used for debug
+
 
 void setup() {
-  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-#if defined (__AVR_ATtiny85__)
-  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-#endif
-  // End of trinket special code
-
   pixels.begin(); // This initializes the NeoPixel library.
+  
+  setStatus (11,1);
+  
+    // start serial port at 9600 bps and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  establishContact();  // send a byte to establish contact until receiver responds 
+    
+  setStatus (11, 2);
 }
 
 void loop() {
 
-  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
+  /* // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
 
   for(int i=0;i<NUMPIXELS;i++){
 
     // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
+    pixels.setPixelColor(i, pixels.Color(150,0,0)); // Moderately bright green color.
 
     pixels.show(); // This sends the updated pixel color to the hardware.
 
     delay(delayval); // Delay for a period of time (in milliseconds).
 
+  }*/
+  
+  communicate();
+  
+  setStatus (11,3);
+  
+  for (int i=0; i<6; i++) {
+    setStatus (i, ledStatus[i]+1);
+  }
+  
+  delay(delayval);
+  
+  
+}
+
+
+void communicate () {
+  while (1) {
+      if (Serial.available() > 0) {
+      // wait until receive 254
+      inByte = Serial.read();
+      
+      // If we get a 255 - then reset
+      if (inByte == 255) {
+        setStatus (11, 2);
+        return;
+      }
+      
+      if (inByte != 254) continue;
+      
+      // Must have received 254 - so read next 6 values
+      for (int i=0; i<6; i++) {
+          ledStatus[i] = Serial.read();
+      }
+    
+      Serial.write(254);
+      Serial.write(0);  
+      Serial.write(0);
+      Serial.write(34);
+      Serial.write(35);
+      Serial.write(36);
+      Serial.write(37);
+      
+      break;
+    }  
+    
+  }
+}
+
+
+// set led - status 0 = off, 1 = green, 2 = red, 3 = blue (searching)
+void setStatus (int lednum, int status) {
+ switch (status) {
+  case 0: pixels.setPixelColor(lednum, pixels.Color(0,0,0));
+          break;
+  case 1: pixels.setPixelColor(lednum, pixels.Color(0,150,0));
+          break;
+  case 2: pixels.setPixelColor(lednum, pixels.Color(150,0,0));
+          break;
+  case 3: pixels.setPixelColor(lednum, pixels.Color(0,0,150));
+          break;
+  // default invalid - so give a unusual value
+  default:pixels.setPixelColor(lednum, pixels.Color(150,150,0));
+          break;
+ }
+ pixels.show();
+}
+
+
+void establishContact() {
+  setStatus (0, 0);
+  while (1) {
+    Serial.write(255);   // send an initial string
+    delay(300);
+    // read and check for a 255 indicating that the pi is ready
+    if (Serial.available() > 0) {
+      inByte = Serial.read();
+      commStatus = 1 - commStatus;
+      setStatus (0, commStatus);
+      if (inByte == 255) { 
+        return;
+      }
+      //Serial.write(inByte);
+    }
   }
 }
--- src/network-quiz.py
+++ src/network-quiz.py
@@ -4,32 +4,44 @@ import serial
 import time
 import struct
 
-ser = serial.Serial('/dev/ttyACM3', 9600, timeout=5)
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=5)
 
 # read from Arduino
 input = ser.read()
-#print ("Read input " + input.decode("utf-8") + " from Arduino")
 # Initial response is 255 = ready to send
-ser.write(struct.pack('!B',255))
+ser.write(struct.pack('B',255))
+
+# Used to set timeout
+timer = 0;
 
 while 1:
         # write something back
-        #ser.write(b'A')
-        ser.write(struct.pack('!B',254))
-        ser.write(struct.pack('!B',3))
-        ser.write(struct.pack('!B',2))
-        ser.write(struct.pack('!B',1))
-        ser.write(struct.pack('!B',0))
-        ser.write(struct.pack('!B',2))
-        ser.write(struct.pack('!B',3))
+        #ser.write(b'A') #!b
+        ser.write(struct.pack('B',254))
+        ser.write(struct.pack('B',3))
+        ser.write(struct.pack('B',2))
+        ser.write(struct.pack('B',1))
+        ser.write(struct.pack('B',0))
+        ser.write(struct.pack('B',2))
+        ser.write(struct.pack('B',3))
 
+        timer = 0;
         # Now clear read buffer until get 254
         while 1:
+                if (ser.inWaiting() == 0) :
+                        timer +=1
+                        if (timer == 20) :
+                                ser.write(struct.pack('B',255))
+                                break
+                        print (".", end="")
+                        time.sleep (1)
+                        continue
                 input = ser.read()
                 input_number = ord(input)
-                print ("Clearing buffer " + str(input_number))
+                # print ("Clearing buffer " + str(input_number))
                 if (input_number == 255) : 
-                        ser.write(struct.pack('!B',255))
+                        ser.write(struct.pack('B',255))
+                        continue
                 if (input_number == 254) : 
                         break
 		
@@ -39,6 +51,5 @@ while 1:
                 input = ser.read()
                 input_number = ord(input)
                 print ("Read input back: " + str(input_number))
-                #print ("Read input back: " + input)
 
         time.sleep(5)
